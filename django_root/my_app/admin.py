6fea1836ce87385823bd3d97cc2675d24606fb97
Your Name <you@example.com>
2022-08-20 01:22:26 +0000

    Big model Work

--- django_root/my_app/admin.py
+++ django_root/my_app/admin.py
@@ -5,9 +5,11 @@ from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 
 
 admin.site.register(Category, DraggableMPTTAdmin)
-admin.site.register(Blogpost)
-admin.site.register(Comment)
-admin.site.register(Message)
+# admin.site.register(Item)
+# admin.site.register(Post)
+admin.site.register(Tutorial)
+admin.site.register(Article)
+admin.site.register(Service)
 # admin.site.register(Rabbithole)
 # admin.site.register(Like)
 # admin.site.register(Dislike)
